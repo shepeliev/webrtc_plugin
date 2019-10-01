a5463d3b9d9727a966f6e24e47090c77239f7fce
Oleksandr Shepeliev <a.shepeliev@gmail.com>
2019-10-01 13:27:57 +0300

    Remove unnecessary code

--- lib/src/media/media.dart
+++ lib/src/media/media.dart
@@ -1,2 +1,5 @@
 export './media_stream.dart';
 export './user_media.dart';
+export './media_constraints.dart';
+export './audio.dart';
+export './video.dart';
--- lib/webrtc_plugin.dart
+++ lib/webrtc_plugin.dart
@@ -1,6 +1,4 @@
 export 'src/texture_renderer.dart';
 export 'src/widget/media_stream_view.dart';
-export 'src/media.dart';
 export 'src/media/media.dart';
-export 'src/utils/flexible_range.dart';
 export 'src/peer_connection/peer_connection.dart';
